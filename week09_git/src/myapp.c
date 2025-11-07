e1e3bb1dcba396f9d996140e4d65ab14b790ce37
Daejun Yun <daramg0214@gmail.com>
2025-11-07 17:18:17 +0900

    add main(), print good bye

--- week09_git/src/myapp.c
+++ week09_git/src/myapp.c
@@ -30,6 +30,7 @@ int main(void) {
     printf("Enter expression (e.g. 1 + 2): ");
     scanf("%lf %s %lf", &a, op, &b);
     printf("Result = %.2lf\n", calculate(op, a, b));
+    printf("~~~ Good bye. See you again ~~~\n\n");
 
     return 0;
 }
